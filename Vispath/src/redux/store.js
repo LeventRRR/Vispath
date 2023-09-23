107d77ad741f9eeb845417f34879fd4783e1cf3b
LeventRRR <139729989+LeventRRR@users.noreply.github.com>
2023-09-23 15:47:32 +0800

    Delete Vispath directory

--- Vispath/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-// src/store.js
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers'; // 这里是你的根Reducer
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
