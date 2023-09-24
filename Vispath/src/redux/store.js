3807a32ec5c5921fa22ee2d732c69975bbd47ad8
LeventRRR <139729989+LeventRRR@users.noreply.github.com>
2023-09-24 18:54:27 +0800

    Delete Vispath directory

--- Vispath/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers'; // 这里是你的根Reducer
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
-
-export default store;
